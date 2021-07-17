8496fdf0a349247de5ce1e9a112a3512a22f0b7c
Mariana Ramos <7marianaramos@gmail.com>
2021-07-17 16:53:50 +0100

    ~wheel

--- giveaway_app/lib/screens/profileAwards.dart
+++ giveaway_app/lib/screens/profileAwards.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:chips_choice/chips_choice.dart';
 import 'package:dropdown_formfield/dropdown_formfield.dart';
 import 'package:flutter/material.dart';
@@ -7,6 +10,7 @@ import 'package:smartcon_app/screens/profile.dart';
 import 'package:smartcon_app/services/database.dart';
 
 import 'conferenceSuggestions/donate.dart';
+import 'package:flutter_spinning_wheel/flutter_spinning_wheel.dart';
 
 class ProfileAwards extends StatelessWidget {
   @override
@@ -96,7 +100,7 @@ class ProfileAwards extends StatelessWidget {
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => Donate()),
+                  MaterialPageRoute(builder: (context) => Wheel()),
                 );
               },
               child: Text(
@@ -108,5 +112,91 @@ class ProfileAwards extends StatelessWidget {
         ),
       ])),
     );
+  }
+}
+
+class SpiningWheel extends StatefulWidget {
+  @override
+  _SpiningWheelState createState() => _SpiningWheelState();
+}
+
+class _SpiningWheelState extends State<SpiningWheel> {
+  final StreamController _dividerController = StreamController<int>();
+
+  final _wheelNotifier = StreamController<double>();
+  @override
+  void dispose() {
+    super.dispose();
+    _dividerController.close();
+    _wheelNotifier.close();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Color(0xffDDC3FF), elevation: 0.0),
+      backgroundColor: Color(0xffDDC3FF),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SpinningWheel(
+              Image.asset('images/roulette.png'),
+              width: 310,
+              height: 310,
+              initialSpinAngle: _generateRandomAngle(),
+              spinResistance: 0.6,
+              canInteractWhileSpinning: false,
+              dividers: 8,
+              onUpdate: _dividerController.add,
+              onEnd: _dividerController.add,
+              secondaryImage: Image.asset('images/roulettecenter.png'),
+              secondaryImageHeight: 110,
+              secondaryImageWidth: 110,
+              shouldStartOrStop: _wheelNotifier.stream,
+            ),
+            SizedBox(height: 30),
+            StreamBuilder(
+              stream: _dividerController.stream,
+              builder: (context, snapshot) =>
+                  snapshot.hasData ? RouletteScore(snapshot.data) : Container(),
+            ),
+            SizedBox(height: 30),
+            new ElevatedButton(
+              child: new Text("Start"),
+              onPressed: () =>
+                  _wheelNotifier.sink.add(_generateRandomVelocity()),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  double _generateRandomVelocity() => (Random().nextDouble() * 6000) + 2000;
+
+  double _generateRandomAngle() => Random().nextDouble() * pi * 2;
+}
+
+class RouletteScore extends StatelessWidget {
+  final int selected;
+
+  final Map<int, String> labels = {
+    1: '1000\$',
+    2: '400\$',
+    3: '800\$',
+    4: '7000\$',
+    5: '5000\$',
+    6: '300\$',
+    7: '2000\$',
+    8: '100\$',
+  };
+
+  RouletteScore(this.selected);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('${labels[selected]}',
+        style: TextStyle(fontStyle: FontStyle.italic, fontSize: 24.0));
   }
 }
